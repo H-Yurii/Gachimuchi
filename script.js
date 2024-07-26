ff178e5c46f20b8f92e8b03a1bd5c96cac8890db
H-Yurii <134538109+H-Yurii@users.noreply.github.com>
2024-07-26 20:18:31 +0200

    First Try

---
+++ script.js
@@ -0,0 +1,34 @@
+let slider = document.querySelector('.slider')
+let sliderSlide = document.querySelectorAll('.slider__slide')
+let buttons = document.querySelectorAll('.slider__nav-button')
+let currentIndex = 0
+let leftSlide = document.querySelector('.prev__slide')
+let rightSlide = document.querySelector('.next__slide')
+let body = document.querySelector('.body')
+let colors = ['#e0af6b', 'gray', '#1257b7', '#aae16f']
+
+function showSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    body.style.backgroundColor = colors[index]
+}
+
+function goToSlide(index) {
+    currentIndex = index;
+    showSlide(currentIndex);
+}
+
+function goToNextSlide() {
+    currentIndex = (currentIndex + 1) % sliderSlide.length;
+    showSlide(currentIndex);
+}
+
+function goToPrevSlide() {
+    currentIndex = (currentIndex - 1 + sliderSlide.length) % sliderSlide.length;
+    showSlide(currentIndex);
+}
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => goToSlide(index));
+});
+
+showSlide(currentIndex)
